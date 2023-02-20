26d429afe03a641892df2ab37ca4fd6cf9accb82
chaitanya8080 <girase.chaitanya@gmail.com>
2023-02-20 15:38:53 +0530

    logout credentials

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,6 +74,9 @@ export const logout = async (req, res) => {
       .status(200)
       .cookie("token", null, {
         expires: new Date(Date.now()),
+        httpOnly:true,
+        secure:true,
+        sameSite:"none"
       })
       .json({ success: true, message: "Logged Out Successfully" });
   } catch (error) {
