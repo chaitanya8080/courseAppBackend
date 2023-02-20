a5faf263c67252a7c369cea1022faabcc06d7158
chaitanya8080 <girase.chaitanya@gmail.com>
2023-02-20 11:40:08 +0530

    cors added

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
+import express from "express";
 
+import { config } from "dotenv";
 
-import express from 'express';
+import Course from "./routes/courseRoute.js";
+import User from "./routes/userRoutes.js";
+import cookiesParser from "cookie-parser";
+import Payment from "./routes/paymentRoutes.js";
+import other from "./routes/otherRoutes.js";
+import cors from "cors";
 
- import {config} from 'dotenv';
-
- import Course from './routes/courseRoute.js';
- import User from './routes/userRoutes.js'
- import cookiesParser from "cookie-parser";
-import Payment from "./routes/paymentRoutes.js"
-import other from "./routes/otherRoutes.js"
-
-
- config({
-    path:'./config/config.env',
- })
+config({
+  path: "./config/config.env",
+});
 
 const app = express();
 //routes
-app.use(express.json())
-app.use(express.urlencoded({
-   extended:true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(cookiesParser());
 
+app.use(cors({
+   origin:process.env.FRONTEND_URL,
+   credentials:true,
+   methods:["GET","POST","PUT","DELETE"],
+}))
 
-app.use("/api/v1",Course)
-app.use("/api/v1",User)
-app.use("/api/v1", Payment)
-app.use("/api/v1", other)
-
-
+app.use("/api/v1", Course);
+app.use("/api/v1", User);
+app.use("/api/v1", Payment);
+app.use("/api/v1", other);
 
 export default app;
 
+app.use("/", (req, res) =>
+  res.send(
+    `<h1>site is working <a href=${process.env.FRONTEND_URL}>Click here</a> to visit frontend</h1>`
+  )
+);
